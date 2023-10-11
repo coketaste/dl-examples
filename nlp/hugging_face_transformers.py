d222db2afd1357a1675016149751cb2e18665196
coketaste <coketaste>
2023-10-11 04:10:02 +0000

    Implemented tokenization, training, and fine tuning.

--- nlp/hugging_face_transformers.py
+++ nlp/hugging_face_transformers.py
@@ -14,9 +14,11 @@
 
 # COMMAND ----------
 
-!pip install transformers
+!pip install --upgrade pip
+!pip install transformers[torch]
 !pip install datasets
 !pip install torch
+!pip install accelerate -U
 
 # COMMAND ----------
 
@@ -87,3 +89,459 @@ print(f"The prediction is {labels[prediction]}")
 # MAGIC Pretrained models are implemented along with tokenizers that are used to preprocess their inputs. The tokenizers take raw strings or list of strings and output what are effectively dictionaries that contain the the model inputs.
 # MAGIC
 # MAGIC You can access tokenizers either with the Tokenizer class specific to the model you want to use (here DistilBERT), or with the AutoTokenizer class. Fast Tokenizers are written in Rust, while their slow versions are written in Python.
+
+# COMMAND ----------
+
+from transformers import DistilBertTokenizer, DistilBertTokenizerFast, AutoTokenizer
+
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-cased")      # written in Python
+print(tokenizer)
+tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-cased")  # written in Rust
+print(tokenizer)
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased") # convenient! Defaults to Fast
+print(tokenizer)
+
+# COMMAND ----------
+
+# This is how you call the tokenizer
+input_str = "Hugging Face Transformers is great!"
+tokenized_inputs = tokenizer(input_str)
+
+
+print("Vanilla Tokenization")
+print_encoding(tokenized_inputs)
+print()
+
+# Two ways to access:
+print(tokenized_inputs.input_ids)
+print(tokenized_inputs["input_ids"])
+
+# COMMAND ----------
+
+cls = [tokenizer.cls_token_id]
+sep = [tokenizer.sep_token_id]
+
+# Tokenization happens in a few steps:
+input_tokens = tokenizer.tokenize(input_str)
+input_ids = tokenizer.convert_tokens_to_ids(input_tokens)
+input_ids_special_tokens = cls + input_ids + sep
+
+decoded_str = tokenizer.decode(input_ids_special_tokens)
+
+print("start:                ", input_str)
+print("tokenize:             ", input_tokens)
+print("convert_tokens_to_ids:", input_ids)
+print("add special tokens:   ", input_ids_special_tokens)
+print("--------")
+print("decode:               ", decoded_str)
+
+# NOTE that these steps don't create the attention mask or add the special characters
+
+# COMMAND ----------
+
+# For Fast Tokenizers, there's another option too:
+inputs = tokenizer._tokenizer.encode(input_str)
+
+print(input_str)
+print("-"*5)
+print(f"Number of tokens: {len(inputs)}")
+print(f"Ids: {inputs.ids}")
+print(f"Tokens: {inputs.tokens}")
+print(f"Special tokens mask: {inputs.special_tokens_mask}")
+print()
+print("char_to_word gives the wordpiece of a character in the input")
+char_idx = 8
+print(f"For example, the {char_idx + 1}th character of the string is '{input_str[char_idx]}',"+\
+      f" and it's part of wordpiece {inputs.char_to_token(char_idx)}, '{inputs.tokens[inputs.char_to_token(char_idx)]}'")
+
+# COMMAND ----------
+
+# Other cool tricks:
+# The tokenizer can return pytorch tensors
+model_inputs = tokenizer("Hugging Face Transformers is great!", return_tensors="pt")
+print("PyTorch Tensors:")
+print_encoding(model_inputs)
+
+# COMMAND ----------
+
+# You can pass multiple strings into the tokenizer and pad them as you need
+model_inputs = tokenizer(["Hugging Face Transformers is great!",
+                         "The quick brown fox jumps over the lazy dog." +\
+                         "Then the dog got up and ran away because she didn't like foxes.",
+                         ],
+                         return_tensors="pt",
+                         padding=True,
+                         truncation=True)
+print(f"Pad token: {tokenizer.pad_token} | Pad token id: {tokenizer.pad_token_id}")
+print("Padding:")
+print_encoding(model_inputs)
+
+# COMMAND ----------
+
+# You can also decode a whole batch at once:
+print("Batch Decode:")
+print(tokenizer.batch_decode(model_inputs.input_ids))
+print()
+print("Batch Decode: (no special characters)")
+print(tokenizer.batch_decode(model_inputs.input_ids, skip_special_tokens=True))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC For more information about tokenizers, you can look at:
+# MAGIC [Hugging Face Transformers Docs](https://huggingface.co/docs/transformers/main_classes/tokenizer) and the [Hugging Face Tokenizers Library](https://huggingface.co/docs/tokenizers/python/latest/quicktour.html) (For the Fast Tokenizers). The Tokenizers Library even lets you train your own tokenizers!
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 0.2 Models
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC Initializing models is very similar to initializing tokenizers. You can either use the model class specific to your model or you can use an AutoModel class. I tend to prefer AutoModel, especially when I want to compare models, because it's easy to specify the models as strings.
+# MAGIC
+# MAGIC While most of the pretrained transformers have similar architecture, if you there are additional weights, called "heads" that you have to train if you're doing sequence classification, question answering, or some other task. Hugging Face automatically sets up the architecture you need when you specify the model class. For example, we are doing sentiment analysis, so we are going to use `DistilBertForSequenceClassification`. If we were going to continue training DistilBERT on its masked-language modeling training objective, we would use `DistilBertForMaskedLM`, and if we just wanted the model's representations, maybe for our own downstream task, we could just use `DistilBertModel`.
+# MAGIC
+# MAGIC
+# MAGIC Here's a stylized picture of a model recreated from one found here: [https://huggingface.co/course/chapter2/2?fw=pt](https://huggingface.co/course/chapter2/2?fw=pt).
+# MAGIC ![model_illustration.png](attachment:model_illustration.png)
+# MAGIC
+# MAGIC
+# MAGIC Here are some examples.
+# MAGIC ```
+# MAGIC *
+# MAGIC *ForMaskedLM
+# MAGIC *ForSequenceClassification
+# MAGIC *ForTokenClassification
+# MAGIC *ForQuestionAnswering
+# MAGIC *ForMultipleChoice
+# MAGIC ...
+# MAGIC ```
+# MAGIC where `*` can be `AutoModel` or a specific pretrained model (e.g. `DistilBert`)
+# MAGIC
+# MAGIC
+# MAGIC There are three types of models:
+# MAGIC * Encoders (e.g. BERT)
+# MAGIC * Decoders (e.g. GPT2)
+# MAGIC * Encoder-Decoder models (e.g. BART or T5)
+# MAGIC
+# MAGIC The task-specific classes you have available depend on what type of model you're dealing with.
+# MAGIC
+# MAGIC
+# MAGIC A full list of choices are available in the [docs](https://huggingface.co/docs/transformers/model_doc/auto). Note that not all models are compatible with all model architectures, for example DistilBERT is not compatible with the Seq2Seq models because it only consists of an encoder.
+
+# COMMAND ----------
+
+from transformers import AutoModelForSequenceClassification, DistilBertForSequenceClassification
+
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cased', num_labels=2)
+model = AutoModelForSequenceClassification.from_pretrained('distilbert-base-cased', num_labels=2)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We get a warning here because the sequence classification parameters haven't been trained yet.
+# MAGIC
+# MAGIC Passing inputs to the model is super easy. They take inputs as keyword arguments
+
+# COMMAND ----------
+
+model_inputs = tokenizer(input_str, return_tensors="pt")
+
+# Option 1
+model_outputs = model(input_ids=model_inputs.input_ids, attention_mask=model_inputs.attention_mask)
+
+# Option 2 - the keys of the dictionary the tokenizer returns are the same as the keyword arguments
+#            the model expects
+
+# f({k1: v1, k2: v2}) = f(k1=v1, k2=v2)
+
+model_outputs = model(**model_inputs)
+
+print(model_inputs)
+print()
+print(model_outputs)
+print()
+print(f"Distribution over labels: {torch.softmax(model_outputs.logits, dim=1)}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC If you notice, it's a bit weird that we have two classes for a binary classification task - you could easily have a single class and just choose a threshold. It's like this because of how huggingface models calculate the loss. This will increase the number of parameters we have, but shouldn't otherwise affect performance.
+# MAGIC
+# MAGIC These models are just Pytorch Modules! You can calculate the loss with your loss_func and call loss.backward. You can use any of the optimizers or learning rate schedulers that you used
+
+# COMMAND ----------
+
+# You can calculate the loss like normal
+label = torch.tensor([1])
+loss = torch.nn.functional.cross_entropy(model_outputs.logits, label)
+print(loss)
+loss.backward()
+
+# You can get the parameters
+list(model.named_parameters())[0]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Hugging Face provides an additional easy way to calculate the loss as well:
+
+# COMMAND ----------
+
+# To calculate the loss, we need to pass in a label:
+model_inputs = tokenizer(input_str, return_tensors="pt")
+
+labels = ['NEGATIVE', 'POSITIVE']
+model_inputs['labels'] = torch.tensor([1])
+
+model_outputs = model(**model_inputs)
+
+
+print(model_outputs)
+print()
+print(f"Model predictions: {labels[model_outputs.logits.argmax()]}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC One final note - you can get the hidden states and attention weights from the models really easily. This is particularly helpful if you're working on an analysis project. (For example, see [What does BERT look at?](https://arxiv.org/abs/1906.04341)).
+
+# COMMAND ----------
+
+from transformers import AutoModel
+
+model = AutoModel.from_pretrained("distilbert-base-cased", output_attentions=True, output_hidden_states=True)
+model.eval()
+
+model_inputs = tokenizer(input_str, return_tensors="pt")
+with torch.no_grad():
+    model_output = model(**model_inputs)
+
+
+print("Hidden state size (per layer):  ", model_output.hidden_states[0].shape)
+print("Attention head size (per layer):", model_output.attentions[0].shape)     # (layer, batch, query_word_idx, key_word_idxs)
+                                                                               # y-axis is query, x-axis is key
+print(model_output)    
+
+# COMMAND ----------
+
+tokens = tokenizer.convert_ids_to_tokens(model_inputs.input_ids[0])
+print(tokens)
+
+n_layers = len(model_output.attentions)
+n_heads = len(model_output.attentions[0][0])
+fig, axes = plt.subplots(6, 12)
+fig.set_size_inches(18.5*2, 10.5*2)
+for layer in range(n_layers):
+    for i in range(n_heads):
+        axes[layer, i].imshow(model_output.attentions[layer][0, i])
+        axes[layer][i].set_xticks(list(range(10)))
+        axes[layer][i].set_xticklabels(labels=tokens, rotation="vertical")
+        axes[layer][i].set_yticks(list(range(10)))
+        axes[layer][i].set_yticklabels(labels=tokens)
+
+        if layer == 5:
+            axes[layer, i].set(xlabel=f"head={i}")
+        if i == 0:
+            axes[layer, i].set(ylabel=f"layer={layer}")
+            
+plt.subplots_adjust(wspace=0.3)
+plt.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Part 1: Finetuning
+# MAGIC
+# MAGIC For your projects, you are much more likely to want to finetune a pretrained model. This is a little bit more involved, but is still quite easy.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 2.1 Loading in a dataset
+# MAGIC
+# MAGIC In addition to having models, the [the hub](https://huggingface.co/datasets) also has datasets.
+
+# COMMAND ----------
+
+from datasets import load_dataset, DatasetDict
+
+# DataLoader(zip(list1, list2))
+
+imdb_dataset = load_dataset("imdb")
+
+# Just take the first 50 tokens for speed/running on cpu
+def truncate(example):
+    return {
+        'text': " ".join(example['text'].split()[:50]),
+        'label': example['label']
+    }
+
+# Take 128 random examples for train and 32 validation
+small_imdb_dataset = DatasetDict(
+    train=imdb_dataset['train'].shuffle(seed=1111).select(range(128)).map(truncate),
+    val=imdb_dataset['train'].shuffle(seed=1111).select(range(128, 160)).map(truncate),
+)
+
+# COMMAND ----------
+
+small_imdb_dataset
+
+# COMMAND ----------
+
+small_imdb_dataset['train'][:10]
+
+# COMMAND ----------
+
+# Prepare the dataset - this tokenizes the dataset in batches of 16 examples.
+small_tokenized_dataset = small_imdb_dataset.map(
+    lambda example: tokenizer(example['text'], padding=True, truncation=True),
+    batched=True,
+    batch_size=16
+)
+
+small_tokenized_dataset = small_tokenized_dataset.remove_columns(["text"])
+small_tokenized_dataset = small_tokenized_dataset.rename_column("label", "labels")
+small_tokenized_dataset.set_format("torch")
+
+# COMMAND ----------
+
+small_tokenized_dataset['train'][0:2]
+
+# COMMAND ----------
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(small_tokenized_dataset['train'], batch_size=16)
+eval_dataloader = DataLoader(small_tokenized_dataset['val'], batch_size=16)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 2.2 Training
+# MAGIC
+# MAGIC To train your models, you can just use the same kind of training loop that you would use in Pytorch. Hugging Face models are also `torch.nn.Module`s so backpropagation happens the same way and you can even use the same optimizers. Hugging Face also includes optimizers and learning rate schedules that were used to train Transformer models, so you can use these too.
+# MAGIC
+# MAGIC For optimization, we're using the AdamW Optimizer, which is almost identical to Adam except it also includes weight decay.
+# MAGIC And we're using a linear learning rate scheduler, which reduces the learning rate a little bit after each training step over the course of training.
+# MAGIC
+# MAGIC There are other optimizers and learning rate schedulers you can use, but these are the default. If you want to explore, you can look at the ones [Hugging Face offers](https://huggingface.co/docs/transformers/main_classes/optimizer_schedules#schedules), the ones available through [Pytorch](https://pytorch.org/docs/stable/optim.html#how-to-adjust-learning-rate) (e.g. [ReduceLROnPlateau](https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.ReduceLROnPlateau.html), which only decreases the learning rate when the validation loss stops decreasing), or write your own (like the one in Assignment 4).
+
+# COMMAND ----------
+
+from transformers import AdamW, get_linear_schedule_with_warmup
+from tqdm.notebook import tqdm
+
+
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cased', num_labels=2)
+
+num_epochs = 3
+num_training_steps = 3 * len(train_dataloader)
+optimizer = AdamW(model.parameters(), lr=5e-5, weight_decay=0.01)
+lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+
+best_val_loss = float("inf")
+progress_bar = tqdm(range(num_training_steps))
+for epoch in range(num_epochs):
+    # training
+    model.train()
+    for batch_i, batch in enumerate(train_dataloader):
+        
+        # batch = ([text1, text2], [0, 1])
+
+        output = model(**batch)
+        
+        optimizer.zero_grad()
+        output.loss.backward()
+        optimizer.step()
+        lr_scheduler.step()
+        progress_bar.update(1)
+    
+    # validation
+    model.eval()
+    for batch_i, batch in enumerate(eval_dataloader):
+        with torch.no_grad():
+            output = model(**batch)
+        loss += output.loss
+    
+    avg_val_loss = loss / len(eval_dataloader)
+    print(f"Validation loss: {avg_val_loss}")
+    if avg_val_loss < best_val_loss:
+        print("Saving checkpoint!")
+        best_val_loss = avg_val_loss
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'val_loss': best_val_loss,
+            },
+            f"/Repos/ysha@amd.com/dl-examples/nlp/checkpoints/epoch_{epoch}.pt"
+        )  
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC While you can use PyTorch to train your models. Hugging Face offers a powerful Trainer class to handle most needs. I think it works pretty well, though there are some customizations I'd recommend.
+
+# COMMAND ----------
+
+imdb_dataset = load_dataset("imdb")
+
+small_imdb_dataset = DatasetDict(
+    train=imdb_dataset['train'].shuffle(seed=1111).select(range(128)).map(truncate),
+    val=imdb_dataset['train'].shuffle(seed=1111).select(range(128, 160)).map(truncate),
+)
+
+small_tokenized_dataset = small_imdb_dataset.map(
+    lambda example: tokenizer(example['text'], truncation=True),
+    batched=True,
+    batch_size=16
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC `TrainingArguments` specifies different training parameters like how often to evaluate and save model checkpoints, where to save them, etc. There are **many** aspects you can customize and it's worth checking them out [here](https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments). Some things you can control include:
+# MAGIC * learning rate, weight decay, gradient clipping, 
+# MAGIC * checkpointing, logging, and evaluation frequency
+# MAGIC * where you log to (default is tensorboard, but if you use WandB or MLFlow they have integrations)
+# MAGIC
+# MAGIC The `Trainer` actually performs the training. You can pass it the `TrainingArguments`, model, the datasets, tokenizer, optimizer, and even model checkpoints to resume training from. The `compute_metrics` function is called at the end of evaluation/validation to calculate evaluation metrics.
+
+# COMMAND ----------
+
+from transformers import TrainingArguments, Trainer
+
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-cased', num_labels=2)
+
+arguments = TrainingArguments(
+    output_dir="sample_hf_trainer",
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    evaluation_strategy="epoch", # run validation at the end of each epoch
+    save_strategy="epoch",
+    learning_rate=2e-5,
+    load_best_model_at_end=True,
+    seed=224
+)
+
+
+def compute_metrics(eval_pred):
+    """Called at the end of validation. Gives accuracy"""
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    # calculates the accuracy
+    return {"accuracy": np.mean(predictions == labels)}
+
+
+trainer = Trainer(
+    model=model,
+    args=arguments,
+    train_dataset=small_tokenized_dataset['train'],
+    eval_dataset=small_tokenized_dataset['val'], # change to test when you do your final evaluation!
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
